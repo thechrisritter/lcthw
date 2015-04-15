503fef878ff84d7ca9d0dde2132b3e4c9f30d1f6
Chris Ritter <thechrisritter@gmail.com>
2015-04-15 14:47:34 -0500

    Create ex3_drills.py

---
+++ ex3.py/ex3_drills.py
@@ -0,0 +1,37 @@
+# prints the string
+print "I will now count my chickens:"
+
+# prints Hens and does math
+print "Hens", 25 + 30 / 6
+# prints Roosters and does math
+print "Roosters", 100 - 25 * 3 % 4
+
+# prints the string
+print "Now I will count the eggs:"
+
+# does a bunch of math
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# prints the string
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# does the math from the previous string
+print 3 + 2 < 5 - 7
+
+# prints the string
+print "What is 3 + 2?", 3 + 2"
+# prints the string
+print "What is 5 - 7?", 5 - 7
+
+# prints the string
+print "Oh, that's wy it's False."
+
+# prints the string
+print "How about some more?"
+
+# prints the string, does some math
+print "Is it greater?, 5 > -2
+# prints the string, does some math
+print " Is it greater or equal?", 5 >= -2
+# prints the string does some math
+print "Is it less or equal?", 5 <= -2
